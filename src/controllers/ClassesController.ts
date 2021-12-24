8dc78f76882c110c1ef0b4c8c43149fa86fd4a7f
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2021-12-24 16:20:45 -0300

    list all clasess route

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -33,28 +33,43 @@ export default class ClassesController {
 
     const filters = request.query;
 
-    const subject = filters.subject as string;
-    const week_day = filters.week_day as string;
-    const time = filters.time as string;
+    if (filters.subject && filters.week_day && filters.time) {
+      const subject = filters.subject as string;
+      const week_day = filters.week_day as string;
+      const time = filters.time as string;
 
-    if (!filters.week_day || !filters.subject || !filters.time) {
-      return response.status(400).json({
-        error: 'Missing filters to search classes',
-      });
+      const timeInMinutes = covertHourToMinutes(time);
+
+      const classes = await db('classes')
+        .whereExists(function () {
+          this.select('class_schedule.*')
+            .from('class_schedule')
+            .whereRaw('class_schedule.class_id = classes.id')
+            .whereRaw('class_schedule.week_day = ??', [Number(week_day)])
+            .whereRaw('class_schedule.from <= ?? ', [timeInMinutes])
+            .whereRaw('class_schedule.to > ?? ', [timeInMinutes]);
+        })
+        .where('classes.subject', '=', subject)
+        .join('users', 'classes.user_id', '=', 'users.id')
+        .select([
+          'classes.id as class_id',
+          'classes.subject',
+          'classes.cost',
+          'users.id as user_id',
+          'users.name',
+          'users.avatar',
+          'users.bio',
+          'users.email',
+          'users.surname',
+          'users.whatsapp',
+        ])
+        .limit(5)
+        .offset((Number(page) - 1) * 5);
+
+      return response.json(classes);
     }
 
-    const timeInMinutes = covertHourToMinutes(time);
-
     const classes = await db('classes')
-      .whereExists(function () {
-        this.select('class_schedule.*')
-          .from('class_schedule')
-          .whereRaw('class_schedule.class_id = classes.id')
-          .whereRaw('class_schedule.week_day = ??', [Number(week_day)])
-          .whereRaw('class_schedule.from <= ?? ', [timeInMinutes])
-          .whereRaw('class_schedule.to > ?? ', [timeInMinutes]);
-      })
-      .where('classes.subject', '=', subject)
       .join('users', 'classes.user_id', '=', 'users.id')
       .select([
         'classes.id as class_id',
