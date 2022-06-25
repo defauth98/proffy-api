93817063f26fb6f42b14c11251488c5525c7e1ad
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-06-25 10:16:13 -0300

    feat: change variables names

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -4,11 +4,11 @@ import 'dotenv/config';
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-    port: Number(process.env.PG_PORT),
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    port: Number(process.env.PGPORT),
   },
 
   useNullAsDefault: true,
